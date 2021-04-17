4031e69afc33eec6d06797ee05884a7596c042cd
shweta0291 <74100955+shweta0291@users.noreply.github.com>
2021-04-17 23:59:27 +0100

    Update config.php

--- hms_admin/include/config.php
+++ hms_admin/include/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'root');
+define('DB_SERVER','hms.cmhsinxtjx3j.us-east-1.rds.amazonaws.com');
+define('DB_USER','admin');
+define('DB_PASS' ,'Shweta1234');
 define('DB_NAME', 'hms');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
